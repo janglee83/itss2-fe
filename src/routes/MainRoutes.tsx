0e4d6a4631b99ffdc71bbc0a71a3f5bc92e4f089
giangleee <giangle602@gmail.com>
2023-12-09 16:29:54 +0700

    Fix error

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -2,7 +2,7 @@ import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 // import page
-const HomePage = lazy(async () => await import("@/pages/Home"));
+const HomePage = lazy(async () => await import("pages/Home"));
 
 export const MainRoutes = createBrowserRouter([
   {
---
+++ src/services/analytics.ts
@@ -0,0 +1,1 @@
+export const IS_ADMIN: number = 1;
