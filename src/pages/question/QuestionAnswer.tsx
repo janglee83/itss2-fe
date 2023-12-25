fc61b20c662d94f434af9c28d96707b6b872ad34
Nguyen Hanh <hanh1632001@gmail.com>
2023-12-25 22:25:53 +0700

    feature: accept answer

--- src/pages/question/QuestionAnswer.tsx
+++ src/pages/question/QuestionAnswer.tsx
@@ -1,22 +1,56 @@
-import UserAvatar from "assets/svg/question/UserAvatar";
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { type IAnswerDetail } from "@/state/questionDetail/state";
+import { type AppDispatch } from "@/state/store";
+import CheckSvg from "assets/svg/question/CheckSvg";
 import HeartSvg from "assets/svg/question/HeartSvg";
 import StarSvg from "assets/svg/question/StarSvg";
-import { useEffect, type FunctionComponent } from "react";
-import CheckSvg from "assets/svg/question/CheckSvg";
-import { type IAnswerDetail } from "@/state/questionDetail/state";
+import UserAvatar from "assets/svg/question/UserAvatar";
+import { useEffect, type FunctionComponent, startTransition } from "react";
+import { useDispatch } from "react-redux";
+import { acceptAnswer } from "../../state/questionDetail/reducers";
+import { setIsLoading } from "state/universe";
+import { useNavigate } from "react-router-dom";
 
 interface IQuestionAnswer {
   onRender: () => void;
   answer: IAnswerDetail;
+  acceptedAnswerId: number | null;
+  questionId: string | undefined;
 }
 
 const QuestionAnswer: FunctionComponent<IQuestionAnswer> = ({
   onRender,
   answer,
+  acceptedAnswerId,
+  questionId,
 }) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+
   useEffect(() => {
     onRender();
   }, []);
+
+  const handleAcceptAnswer = (): void => {
+    dispatch(setIsLoading(true));
+    const payload = {
+      userId: 1,
+      answerId: answer.id,
+    };
+    dispatch(acceptAnswer(payload))
+      .then(() => {
+        startTransition(() => {
+          navigate(`/question/detail/${questionId}`);
+        });
+      })
+      .catch((error) => {
+        console.log((error as Error).message);
+      })
+      .finally(() => {
+        dispatch(setIsLoading(false));
+      });
+  };
 
   return (
     <div className="text-button-primary rounded-lg bg-character-primaryinverse py-3 pr-0 pl-3 border-[1px] border-solid border-gray-400 flex my-[18px]">
@@ -47,10 +81,21 @@ const QuestionAnswer: FunctionComponent<IQuestionAnswer> = ({
             <HeartSvg className="cursor-pointer" />
             <div className="relative leading-[22px]">{answer.likeCount}</div>
           </div>
-          <div className="text-accepted leading-[22px] flex items-center gap-[4px]">
-            <CheckSvg />
-            <div>Câu trả lời được chấp nhận</div>
-          </div>
+          {acceptedAnswerId === answer.id && (
+            <div className="text-accepted leading-[22px] flex items-center gap-[4px]">
+              <CheckSvg />
+              <div>Câu trả lời được chấp nhận</div>
+            </div>
+          )}
+          {acceptedAnswerId === null && (
+            <div
+              className="text-neutral-6 leading-[22px] flex items-center gap-[4px] cursor-pointer hover:text-accepted fill-neutral-6 hover:fill-accepted"
+              onClick={handleAcceptAnswer}
+            >
+              <CheckSvg />
+              <div>Chấp nhận</div>
+            </div>
+          )}
         </div>
       </div>
     </div>
